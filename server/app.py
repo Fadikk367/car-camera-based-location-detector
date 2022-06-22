92994440907285f81df7cfb8cfa19582b659e398
Konrad Walas <kwalas314@gmail.com>
2022-06-22 20:31:20 +0200

    Clean code

--- server/app.py
+++ server/app.py
@@ -10,30 +10,31 @@ CORS(app)
 if __name__ == '__main__':
     app.run()
 
+weights = {
+    'plate': 0.2
+}
+
 @app.route('/', methods=['GET', 'POST'])
 def predict():
-    if request.method == 'POST':
-        # Get file
-        try:
-            image = get_file_from_request()
-        except WrongFileExtension:
-            return {'message': 'Wrong file extension'}, 400
-        data = extract_data_from_plates(image)
+    if request.method == 'GET':
+        return upload_form
+    data = {
+        'countries': {},
+        'markers': []
+    }
 
-        # plate -> lista krajów
+    try:
+        image = get_file_from_request()
+    except WrongFileExtension:
+        return {'message': 'Wrong file extension'}, 400
 
-        # kierunek jazdy -> lista krajów
-        # text extraktor -> lista krajów z prawdopodobieństwami i lista stringów
-        # zapytanie do api czy któryś z tych stringów jest miastem, jak tak to pakumey do markerów
-        # zwracamy
+    
+    for country in extract_data_from_plates(image):
+        data['countries'][country] = weights['plate']
 
-        return {'result': data}, 200
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    # kierunek jazdy -> lista krajów
+    # text extraktor -> lista krajów z prawdopodobieństwami i lista stringów
+    # zapytanie do api czy któryś z tych stringów jest miastem, jak tak to pakumey do markerów
+    # zwracamy
+
+    return {'result': data}, 200
--- server/services.py
+++ server/services.py
@@ -37,3 +37,13 @@ def extract_data_from_plates(image):
 
 class WrongFileExtension(Exception):
     pass
+
+upload_form = '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
