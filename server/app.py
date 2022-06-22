3438986b455b9eddcf00d9f700dff7039dd04645
Konrad Walas <kwalas314@gmail.com>
2022-06-22 20:11:42 +0200

    Remove controllers

--- server/app.py
+++ server/app.py
@@ -1,15 +1,39 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
-from .controllers import api
+
+from .services import *
+from .extractors.plates.plate_analyzer import predict_country_from_plate
+from .extractors.plates.plate_detector import getPlates
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(api)
-
 if __name__ == '__main__':
     app.run()
 
-@app.route('/')
-def hello():
-    return {'message': 'sdfsdfsdf'}
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get file
+        try:
+            image = get_file_from_request()
+        except WrongFileExtension:
+            return {'message': 'Wrong file extension'}, 400
+        for plate in getPlates(image):
+            print(predict_country_from_plate(plate))
+        # plate -> lista krajów
+        # kierunek jazdy -> lista krajów
+        # text extraktor -> lista krajów z prawdopodobieństwami i lista stringów
+        # zapytanie do api czy któryś z tych stringów jest miastem, jak tak to pakumey do markerów
+        # zwracamy
+
+        return {'message': 'Success'}, 200
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
